168e09161bd16b3b9047d9a0e2a3efbe18aed726
Twody2D <paha7a146@gmail.com>
2026-07-08 23:10:44 +0300

    Add server monitoring script

--- linux/info.sh
+++ linux/info.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "========================"
-echo "    SERVER REPORT"
-echo "========================"
+echo "=========================="
+echo "      Server Report"
+echo "=========================="
 echo ""
 
 SERVER_NAME=$(hostname)
-IP_ADDRESS=$(hostname -I)
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
+IP_ADDRESS=$(hostname -I)
+MEM_USAGE=$(free | awk '/Mem:/ {printf "%.0f", $3/$2 * 100}')
 DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
 
 echo -e "Hostname:\n$SERVER_NAME\n"
@@ -16,19 +17,13 @@ echo -e "Date:\n$DATE\n"
 
 echo -e "IP address:\n$IP_ADDRESS\n"
 
-echo "Memory:"
-free -h
-echo ""
+echo -e "Memory usage:\n$MEM_USAGE%\n"
 
-echo "Disk:"
-df -h
-echo ""
-
-if systemctl is-active --quiet nginx
+if [ "$MEM_USAGE" -gt 80 ]
 then
-	echo "Nginx OK"
+        echo "WARNING: Memory usage is high!"
 else
-	echo "Nginx Down"
+        echo "Memory usage OK"
 fi
 echo ""
 
@@ -40,3 +35,12 @@ then
 else
         echo "Disk usage OK"
 fi
+echo ""
+
+if systemctl is-active --quiet nginx
+then
+	echo "Nginx OK"
+else
+	echo "Nginx Down"
+fi
+echo ""
